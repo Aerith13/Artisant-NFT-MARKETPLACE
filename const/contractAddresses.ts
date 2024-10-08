bbe774213972f6663a83a399fadbecdb2d050c64
昀昀 <35630143+Aerith13@users.noreply.github.com>
2024-10-07 20:14:34 -0700

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,17 +2,15 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+import { Sepolia } from "@thirdweb-dev/chains";
+export const NETWORK = Sepolia;
 
 // 2. The address of the marketplace V3 smart contract.
-export const MARKETPLACE_ADDRESS = "0x8030B89fe9ea9d1D7BDBBFb8f36197a9258C15B3";
+export const MARKETPLACE_ADDRESS = "0xAf38e830988a5b3E757F59BeC87B5a3c480c8521";
 
 // 3. The address of our NFT collection smart contract.
-export const NFT_COLLECTION_ADDRESS =
-  "0xCFea4ee5D46E0793Fb8f7871e16888D8787F490C";
-  "0xc514dE329cC87bF7e109343C76CC0ff81F00B2DD";
+export const NFT_COLLECTION_ADDRESS = "0x0A1132a8345d057FeE7e62f079eeBf278293330E"
 
 // Set up the URL of where users can view transactions on
-// we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+// we use Sepolia etherscan to view transactions on the Sepolia EVM testnet.
+export const ETHERSCAN_URL = "https://sepolia.etherscan.io";
